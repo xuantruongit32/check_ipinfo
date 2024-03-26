77ffeaffdb50454c52fa1a1bd9cb74c76265c882
v <xuantruongit32@gmail.com>
2024-03-26 21:44:05 +0700

    feat: add copy button

--- checkip/lib/homePage.dart
+++ checkip/lib/homePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key, required this.ip});
@@ -7,6 +8,14 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void _copyTextToClipBoard(String text, BuildContext context) {
+      Clipboard.setData(
+        ClipboardData(
+          text: text,
+        ),
+      );
+    }
+
     return Scaffold(
         body: SafeArea(
       child: Column(
@@ -18,7 +27,24 @@ class HomePage extends StatelessWidget {
               style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
             ),
           ),
-          Text(ip),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                ip,
+                style: const TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Gap(10),
+              GestureDetector(
+                  onTap: () {
+                    _copyTextToClipBoard(ip, context);
+                  },
+                  child: const Icon(Icons.copy)),
+            ],
+          ),
         ],
       ),
     ));
