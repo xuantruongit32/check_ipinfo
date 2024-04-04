b31a5ab3070fb2300d0570b4815791b4a795de84
v <xuantruongit32@gmail.com>
2024-04-04 21:55:21 +0700

    feat: add textfield

--- checkip/lib/homePage.dart
+++ checkip/lib/homePage.dart
@@ -8,7 +8,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    void _copyTextToClipBoard(String text, BuildContext context) {
+    final controller = TextEditingController();
+    void copyTextToClipBoard(String text, BuildContext context) {
       Clipboard.setData(
         ClipboardData(
           text: text,
@@ -17,36 +18,56 @@ class HomePage extends StatelessWidget {
     }
 
     return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: [
-          const Gap(40),
-          const Center(
-            child: Text(
-              'Your IP Address:',
-              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Gap(40),
+            const Center(
+              child: Text(
+                'Your IP Address:',
+                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                ip,
-                style: const TextStyle(
-                  fontSize: 36,
-                  fontWeight: FontWeight.bold,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  ip,
+                  style: const TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Gap(10),
+                GestureDetector(
+                  onTap: () {
+                    copyTextToClipBoard(ip, context);
+                  },
+                  child: const Icon(Icons.copy),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white),
+                ),
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  keyboardType: const TextInputType.numberWithOptions(),
+                  controller: controller,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'SEARCH IP INFO',
+                  ),
                 ),
               ),
-              const Gap(10),
-              GestureDetector(
-                  onTap: () {
-                    _copyTextToClipBoard(ip, context);
-                  },
-                  child: const Icon(Icons.copy)),
-            ],
-          ),
-        ],
+            )
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
